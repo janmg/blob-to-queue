146724f6f205a83cbf57d7a86ece914da988c077
Jan Geertsma <jan@janmg.com>
2025-04-05 11:55:47 +0300

    update blob

--- input/blob.go
+++ input/blob.go
@@ -20,40 +20,54 @@ func Blobworker(queue chan format.Flatevent) {
 	// TODO: Would need to way to manually change that pointer, incase old files need to be reprocessed
 	// TODO: The worker would need a timer to check for new files or grown files?
 
-	// keep a registry, filelist and a worklist
-	// The registry is the list of files already processed, the filelist is a list of currently seen files, the worklist contains the new files or the files that grew
+	// keep a registry, filelist
+	// The registry is the list of files already processed, the filelist is a list of currently seen files, than read the diffence
 	var registry map[string]int64 = make(map[string]int64)
 	var filelist map[string]int64 = make(map[string]int64)
-	var worklist map[string]int64 = make(map[string]int64)
 
-	interval := time.NewTimer(60 * time.Second)
-	//interval := time.NewTicker(60 * time.Second)
+	// Read the previous timestamp file
+	// Read the registry if it exists
+	fmt.Println(registry)
+
+	//interval := time.NewTimer(60 * time.Second)
+	interval := time.NewTicker(60 * time.Second)
 	defer interval.Stop()
 
-	go func() {
-		<-interval.C
-		fmt.Println("Interval timer fired")
+	for range interval.C {
 		// 1. Lists all the files in the remote storage account that match the path prefix
 		filelist = listFiles(config.Accountname, config.Accountkey, location)
-		// 2. Filters on path_filters to only include files that match the directory and file glob (**/*.json)
-		worklist = registry - filelist
-		// 3. Compare the list of files to the the registry with the new filelist and put the delta in a worklist
+		var fullfiles = 0
+		var partialfiles = 0
 
+		// 2. Filters on path_filters to only include files that match the directory and file glob (**/*.json)
+
+		// 3. Compare the list of files to the the registry with the new filelist and read the
 		// 4. Process the worklist and put all events in the logstash queue.
+		fmt.Println("Listing the blobs in the container:")
+		for name, size := range filelist {
+			if oldSize, exists := registry[name]; !exists || oldSize != size {
+				//convert to log item
+				fmt.Printf("%s grew by %d bytes\n", name, size-oldSize)
+				read(queue, name, oldSize, size)
+				partialfiles++
+			} else {
+				//convert to log item
+				fmt.Printf("%s is new and has %d bytes\n", name, size)
+				read(queue, name, 0, size)
+				fullfiles++
+			}
+		}
+		//convert to log item
+		fmt.Printf("Found %d new files and %d updated files\n", fullfiles, partialfiles)
 
 		// 5. Save the registry with files and sizes to a file
+		saveRegistry(filelist)
 
 		// 6. if there is time left, sleep to complete the interval. If processing takes more than an inteval, save the registry and continue.
 		// ... try to sync the timer to when the files are actually written to the storage account and wait an additional 5 seconds before reading.
 		// ... did storage accounts implement some time of difference tracking journal?
 		// 7. If stop signal comes, finish the current file, save the registry and quit
-
-	}()
-
-	//print(config)
-	// List the blobs in the container
-	fmt.Println("Listing the blobs in the container:")
-	fmt.Println(registry)
+	}
 
 	/*
 		       ChatGPT, suggests to drop events when the queue is full. Obviously would never do that and better to signal a monitor channel to signal to pause reading more data and check regularly if the queue is blocked or not.
@@ -65,25 +79,22 @@ func Blobworker(queue chan format.Flatevent) {
 			   }
 	*/
 
-	// list all the nsg's
-	// resourceId=/SUBSCRIPTIONS/F5DD6E2D-1F42-4F54-B3BD-DBF595138C59/RESOURCEGROUPS/VM/PROVIDERS/MICROSOFT.NETWORK/NETWORKSECURITYGROUPS/
-	// loop through the dates, skip the older ones and process only from the data in the registry
 	/*
-		resourceId=/SUBSCRIPTIONS/F5DD6E2D-1F42-4F54-B3BD-DBF595138C59/RESOURCEGROUPS/VM/PROVIDERS/MICROSOFT.NETWORK/NETWORKSECURITYGROUPS/OCTOBER-NSG/y=2023/m=10/d=31/h=13/m=00/
-
-		for each nsg
-		resourceId=/SUBSCRIPTIONS/F5DD6E2D-1F42-4F54-B3BD-DBF595138C59/RESOURCEGROUPS/VM/PROVIDERS/MICROSOFT.NETWORK/NETWORKSECURITYGROUPS/
-
+		// list all the nsg's
+		// resourceId=/SUBSCRIPTIONS/F5DD6E2D-1F42-4F54-B3BD-DBF595138C59/RESOURCEGROUPS/VM/PROVIDERS/MICROSOFT.NETWORK/NETWORKSECURITYGROUPS/
+		// loop through the dates, skip the older ones and process only from the data in the registry
+			resourceId=/SUBSCRIPTIONS/F5DD6E2D-1F42-4F54-B3BD-DBF595138C59/RESOURCEGROUPS/VM/PROVIDERS/MICROSOFT.NETWORK/NETWORKSECURITYGROUPS/OCTOBER-NSG/y=2023/m=10/d=31/h=13/m=00/
+			for each nsg
+			resourceId=/SUBSCRIPTIONS/F5DD6E2D-1F42-4F54-B3BD-DBF595138C59/RESOURCEGROUPS/VM/PROVIDERS/MICROSOFT.NETWORK/NETWORKSECURITYGROUPS/
 	*/
 
-	// filelist = Hash.new
-	// worklist = Hash.new
-	// @last = start = Time.now.to_i
-	// filelist.clear
-	// filelist = list_blobs(false)
-	// registry.store(name, { :offset => off, :length => file[:length] })
+}
 
-	// save_registry()
+func loadRegistry() map[string]int64 {
+	return make(map[string]int64)
+}
+
+func saveRegistry(filelist map[string]int64) {
 }
 
 func listFiles(account string, key string, location string) map[string]int64 {
@@ -125,6 +136,7 @@ func listFiles(account string, key string, location string) map[string]int64 {
 	return filelist
 }
 
+/*
 func fullRead(queue chan format.Flatevent, name string) {
 	config := common.ConfigHandler()
 	cred, err := azblob.NewSharedKeyCredential(config.Accountname, config.Accountkey)
@@ -134,7 +146,6 @@ func fullRead(queue chan format.Flatevent, name string) {
 	common.Error(err)
 
 	ctx := context.Background()
-	/*
 		// ListBlockBlob
 		blobURL := fmt.Sprintf("https://%s.%s/%s/%s", config.Accountname, config.Cloud, config.ContainerName, name)
 		blockBlobClient, err := blockblob.NewClientWithSharedKeyCredential(blobURL, cred, nil)
@@ -149,7 +160,6 @@ func fullRead(queue chan format.Flatevent, name string) {
 				// QTAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAw
 				// WjAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAw
 			}
-	*/
 
 	get, err := client.DownloadStream(ctx, config.ContainerName, name, nil)
 	common.Error(err)
@@ -163,12 +173,11 @@ func fullRead(queue chan format.Flatevent, name string) {
 	err = retryReader.Close()
 	common.Error(err)
 
-	// parse the json into a flatevent struct and push it into the queue
-	nsgflowlog(queue, downloadedData.Bytes(), name)
-	// vnetflowlog(queue, downloadedData.Bytes(), name)
 }
+*/
 
-func partialRead(queue chan format.Flatevent, name string) {
+// Read the files with the httpRange
+func read(queue chan format.Flatevent, name string, oldSize int64, size int64) {
 	config := common.ConfigHandler()
 	cred, err := azblob.NewSharedKeyCredential(config.Accountname, config.Accountkey)
 	common.Error(err)
@@ -178,17 +187,21 @@ func partialRead(queue chan format.Flatevent, name string) {
 	common.Error(err)
 
 	ctx := context.Background()
-	/*
-		httpRange := azblob.HTTPRange{
-			Offset: 0,
-			Count:  12,
-		}
-		get, err := client.DownloadStream(ctx, config.ContainerName, *blob.Name, &azblob.DownloadStreamOptions{Range: httpRange})
-	*/
-	get, err := client.DownloadStream(ctx, config.ContainerName, name, nil)
+	httpRange := azblob.HTTPRange{
+		Offset: oldSize + 1,
+		Count:  size - oldSize,
+	}
+	get, err := client.DownloadStream(ctx, config.ContainerName, *blob.Name, &azblob.DownloadStreamOptions{Range: httpRange})
+	//get, err := client.DownloadStream(ctx, config.ContainerName, name, nil)
 	common.Error(err)
 
 	downloadedData := bytes.Buffer{}
+	// TODO: account for the missing header in case of partial reads
+	if oldSize > 0 {
+		prefix := []byte("{\"message\": {")
+		downloadedData.Write(prefix)
+	}
+
 	retryReader := get.NewRetryReader(ctx, &azblob.RetryReaderOptions{})
 	_, err = downloadedData.ReadFrom(retryReader)
 	common.Error(err)
@@ -197,7 +210,11 @@ func partialRead(queue chan format.Flatevent, name string) {
 	err = retryReader.Close()
 	common.Error(err)
 
-	// TODO: How to process lines?
+	// TODO: should make a distinction between log types, grok lines, json lines, json structures
+	// parse the json into a flatevent struct and push it into the queue
+	// nsgflowlog(queue, downloadedData.Bytes(), name)
+	// vnetflowlog(queue, downloadedData.Bytes(), name)
+
 	// parse the json into a flatevent struct and push it into the queue
 	nsgflowlog(queue, downloadedData.Bytes(), name)
 }
